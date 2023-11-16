506d71f5eb850bd4140d3e7e0c5abc294155e2b5
Elaidzha1940 <eli080293@gmail.com>
2023-11-16 18:48:48 +0300

    TheCryptoApp

---
+++ TheCryptoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: TheCryptoApp
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 16.11.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheCryptoApp/TheCryptoAppApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: TheCryptoApp
+//  File: TheCryptoAppApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 16.11.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct TheCryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
