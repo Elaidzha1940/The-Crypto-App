accb05293c143fc5b078648a799fb28da1667b07
Elaidzha1940 <eli080293@gmail.com>
2023-11-16 19:11:30 +0300

    Add Color Theme + Extension for Colors

---
+++ TheCryptoApp/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//  /*
+//
+//  Project: TheCryptoApp
+//  File: Color.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 16.11.2023
+//
+//  */
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    
+}
+
+struct ColotTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
