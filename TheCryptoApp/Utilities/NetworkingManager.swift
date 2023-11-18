0bafa1ac3639a09bbf661fdffca4a009adc6e674
Elaidzha1940 <eli080293@gmail.com>
2023-11-18 21:34:56 +0300

    Update NetworkingManager

--- TheCryptoApp/Utilities/NetworkingManager.swift
+++ TheCryptoApp/Utilities/NetworkingManager.swift
@@ -13,20 +13,31 @@ import Combine
 // U don't neet init the class NetworkingManager
 class NetworkingManager {
     
-    
+    enum NetworkingError: LocalizedError {
+        case badUrlResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badUrlResponse(url: let url): "[🔥] Bad response from URl: \(url)"
+            case .unknown: "[⚠️] Unknown error occured."
+                
+            }
+        }
+    }
     
     static func download(url: URL) -> AnyPublisher<Data, any Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap({ try handleURLResponse(output: $0) })
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
-    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
         guard let response = output.response as? HTTPURLResponse,
               response.statusCode >= 200 && response.statusCode < 300 else {
-            throw URLError(.badServerResponse)
+            throw NetworkingError.badUrlResponse(url: url)
         }
         return output.data
     }
