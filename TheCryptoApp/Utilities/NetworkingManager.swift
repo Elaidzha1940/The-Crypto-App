9f879f5c6dafe5b2ed6eeeb5c27309759ed40e07
Elaidzha1940 <eli080293@gmail.com>
2023-11-18 20:27:26 +0300

    Add NetworkingManager

---
+++ TheCryptoApp/Utilities/NetworkingManager.swift
@@ -0,0 +1,18 @@
+//  /*
+//
+//  Project: TheCryptoApp
+//  File: NetworkingManager.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 18.11.2023
+//
+//  */
+
+import Foundation
+
+class NetworkingManager {
+    
+    static func download(url: URL) { // U don't neet init the class NetworkingManager
+         
+    }
+    
+}
