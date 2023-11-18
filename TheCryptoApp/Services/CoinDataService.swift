a1365630c624f8be69deec801e8561f39fe85494
Elaidzha1940 <eli080293@gmail.com>
2023-11-18 21:11:54 +0300

    Update NetworkingManager / CoinDataService

--- TheCryptoApp/Services/CoinDataService.swift
+++ TheCryptoApp/Services/CoinDataService.swift
@@ -26,28 +26,13 @@ class CoinDataService {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
         else { return }
         
-        coinSubscribtion =
-        URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        coinSubscribtion = NetworkingManager.download(url: url)
+        
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { (complition) in
-                switch complition {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 self?.coinSubscribtion?.cancel()
-            }
+            })
+        
     }
 }
--- TheCryptoApp/Utilities/NetworkingManager.swift
+++ TheCryptoApp/Utilities/NetworkingManager.swift
@@ -13,18 +13,30 @@ import Combine
 // U don't neet init the class NetworkingManager
 class NetworkingManager {
     
+    
+    
     static func download(url: URL) -> AnyPublisher<Data, any Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-            }
+            .tryMap({ try handleURLResponse(output: $0) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else {
+            throw URLError(.badServerResponse)
+        }
+        return output.data
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print(error.localizedDescription)
+        }
+    }
 }
