235d5d1dad5831ba1ad0d5e0f5c48f214ac3f2bd
Elaidzha1940 <eli080293@gmail.com>
2023-11-18 20:39:12 +0300

    Update NetworkingManager

--- TheCryptoApp/Services/CoinDataService.swift
+++ TheCryptoApp/Services/CoinDataService.swift
@@ -26,7 +26,8 @@ class CoinDataService {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
         else { return }
         
-        coinSubscribtion = URLSession.shared.dataTaskPublisher(for: url)
+        coinSubscribtion =
+        URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
             .tryMap { (output) -> Data in
                 guard let response = output.response as? HTTPURLResponse,
--- TheCryptoApp/Utilities/NetworkingManager.swift
+++ TheCryptoApp/Utilities/NetworkingManager.swift
@@ -8,11 +8,23 @@
 //  */
 
 import Foundation
+import Combine
 
+// U don't neet init the class NetworkingManager
 class NetworkingManager {
     
-    static func download(url: URL) { // U don't neet init the class NetworkingManager
-         
+    static func download(url: URL) -> AnyPublisher<Data, any Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
     }
     
 }
