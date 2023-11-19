82e4b7af299bec4bae26dc16543a4f6b72937e39
Elaidzha1940 <eli080293@gmail.com>
2023-11-19 18:22:58 +0300

    Update CoinImageViewModel / CoinImageService

--- TheCryptoApp/Core/Components/CoinImage/CoinImageViewModel.swift
+++ TheCryptoApp/Core/Components/CoinImage/CoinImageViewModel.swift
@@ -9,6 +9,7 @@
 
 import Foundation
 import SwiftUI
+import Combine
 
 class CoinImageViewModel: ObservableObject {
     @Published var image: UIImage?
@@ -16,14 +17,24 @@ class CoinImageViewModel: ObservableObject {
     
     private let coin: CoinModel
     private let dataService: CoinImageService
+    private var cancellables = Set<AnyCancellable>()
     
     init(coin: CoinModel) {
         self.coin = coin
         self.dataService = CoinImageService(urlString: coin.image)
-        getImage()
+        self.addSubscribers()
+        self.isLoading = true
     }
     
-    private func getImage() {
+    private func addSubscribers() {
+        
+        dataService.$image
+            .sink { [weak self] (_) in
+                self?.isLoading = false
+            } receiveValue: { [weak self] (returnedImage) in
+                self?.image = returnedImage
+            }
+            .store(in: &cancellables)
         
     }
 }
--- TheCryptoApp/Services/CoinImageService.swift
+++ TheCryptoApp/Services/CoinImageService.swift
@@ -15,15 +15,17 @@ class CoinImageService {
     
     @Published var image: UIImage? = nil
     
-    var imageSubscription: AnyCancellable?
+    private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
     
-    init(urlString: String) {
-        getCoinImage(urlString: urlString)
+    init(coin: CoinModel) {
+        self.coin = coin
+        getCoinImage()
     }
     
-    private func getCoinImage(urlString: String) {
+    private func getCoinImage() {
         
-        guard let url = URL(string: urlString) else { return }
+        guard let url = URL(string: coin.image) else { return }
         
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
