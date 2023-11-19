2c7721e61ba59e2e52dd7adb44f22c3eab0a6237
Elaidzha1940 <eli080293@gmail.com>
2023-11-19 19:25:51 +0300

    Update HomeView

--- TheCryptoApp/Core/Home/Views/CoinRowView.swift
+++ TheCryptoApp/Core/Home/Views/CoinRowView.swift
@@ -46,7 +46,7 @@ extension CoinRowView {
                 .foregroundColor(Color.theme.secondaryText)
                 .frame(minWidth: 30)
             
-            Circle()
+            CoinImageView(coin: coin)
                 .frame(width: 30, height: 30)
             
             Text(coin.symbol.uppercased())
