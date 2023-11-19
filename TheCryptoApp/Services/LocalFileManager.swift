4da0cfc790594a59c7832fec7bdcf693943f7ae8
Elaidzha1940 <eli080293@gmail.com>
2023-11-20 01:42:15 +0300

    Update LocalFileManager

--- TheCryptoApp/Services/LocalFileManager.swift
+++ TheCryptoApp/Services/LocalFileManager.swift
@@ -15,17 +15,33 @@ class LocalFileManager {
     static let instance = LocalFileManager() // Singletone
     private init() { }
     
-    func sameImage(image: UIImage) {
+    func sameImage(image: UIImage, imageName: String, folderName: String) {
         
+        // create folder
+        createFolderIfNeeded(folderName: folderName)
+        
+        // get path for image
         guard
             let data = image.pngData(),
-            let url = URL(string: "")
+            let url = getURLForImage(imageName: imageName, folderName: folderName)
         else { return }
         
+        // save image to path 
         do {
             try data.write(to: url)
         } catch let error {
-            print("Error saving image. \(error)")
+            print("Error saving image. ImageName: \(imageName) \(error)")
+        }
+    }
+    
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getURLForFolder(folderName: folderName) else { return }
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil )
+            } catch let error {
+                print("Error creating directory. FolderName: \(folderName). \(error)")
+            }
         }
     }
     
@@ -41,6 +57,6 @@ class LocalFileManager {
         guard let folderURL = getURLForFolder(folderName: folderName) else {
             return nil
         }
-        return folderURL.appendingPathComponent(imageName)
+        return folderURL.appendingPathComponent(imageName + ".png")
     }
 }
