ab26513fc7278832f67c8fdf65ed9fb5ea180a54
Elaidzha1940 <eli080293@gmail.com>
2023-11-20 18:41:19 +0300

    Add StatisticModel

---
+++ TheCryptoApp/Models/StatisticModel.swift
@@ -0,0 +1,14 @@
+//  /*
+//
+//  Prpject: TheCryptoApp
+//  File: StatisticModel.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 20.11.2023
+//
+//  */
+
+import Foundation
+
+struct StatisticModel {
+    
+}
