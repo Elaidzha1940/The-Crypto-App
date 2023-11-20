a7c83ef584cdb5c9be9c53ec86a6fb4be0b71bc8
Elaidzha1940 <eli080293@gmail.com>
2023-11-20 19:56:06 +0300

    Update StatisticModel

--- TheCryptoApp/Models/StatisticModel.swift
+++ TheCryptoApp/Models/StatisticModel.swift
@@ -9,6 +9,17 @@
 
 import Foundation
 
-struct StatisticModel {
+struct StatisticModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
     
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+    }
 }
+
+let newModel = StatisticModel(title: "", value: "", percentageChange: nil )
