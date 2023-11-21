52f970abdb1e613c5b9a47b7675a65c7f0b25a13
Elaidzha1940 <eli080293@gmail.com>
2023-11-21 19:45:11 +0300

    Add MarketDataService

---
+++ TheCryptoApp/Services/MarketDataService.swift
@@ -0,0 +1,37 @@
+//  /*
+//
+//  Project: TheCryptoApp
+//  File: MarketDataService.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 21.11.2023
+//
+//  */
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    var coinSubscribtion: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h")
+        else { return }
+        
+        coinSubscribtion = NetworkingManager.download(url: url)
+        
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscribtion?.cancel()
+            })
+    }
+}
