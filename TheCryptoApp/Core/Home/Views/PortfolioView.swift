da8aa0b843a02bdbf420656417e33f34c7e94f6f
Elaidzha1940 <eli080293@gmail.com>
2023-11-23 01:53:03 +0300

    Update PortfolioView

--- TheCryptoApp/Core/Home/Views/PortfolioView.swift
+++ TheCryptoApp/Core/Home/Views/PortfolioView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 
 struct PortfolioView: View {
     @EnvironmentObject private var vm: HomeViewModel
+    @State private var selectedCoin: CoinModel? = nil
+    @State private var quantityText: String = ""
     
     var body: some View {
         
@@ -19,14 +21,34 @@ struct PortfolioView: View {
                 VStack(alignment: .leading, spacing: 0) {
                     SearchBarView(searchText: $vm.searchText)
                     
-                    ScrollView(.horizontal, showsIndicators: true, content: {
-                        LazyHStack(spacing: 10) {
-                            ForEach(vm.allCoins) { coin in
-                                CoinLogoView(coin: coin)
-                                    .frame(width: 75)
+                    coinLogoList
+                    
+                    if selectedCoin != nil {
+                        VStack(spacing: 20) {
+                            HStack {
+                                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                                Spacer()
+                                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+                            }
+                            Divider()
+                            HStack {
+                                Text("Amount in your portfolio:")
+                                Spacer()
+                                TextField("Ex: 1.4", text: $quantityText)
+                                    .multilineTextAlignment(.trailing)
+                                    .keyboardType(.decimalPad)
+                            }
+                            Divider()
+                            HStack {
+                                Text("Current value:")
+                                Spacer()
+                                Text(getCurrentValue().asCurrencyWith2Decimals())
                             }
                         }
-                    })
+                        .animation(.none)
+                        .padding()
+                        .font(.system(size: 18, weight: .bold, design: .rounded))
+                    }
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -43,5 +65,41 @@ struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
             .environmentObject(dev.HomeVM)
+    }
+}
+
+extension PortfolioView {
+    private var coinLogoList: some View {
+        
+        ScrollView(.horizontal, showsIndicators: false, content: {
+            LazyHStack(spacing: 10) {
+                ForEach(vm.allCoins) { coin in
+                    CoinLogoView(coin: coin)
+                        .frame(width: 75)
+                        .padding(4)
+                        .onTapGesture {
+                            withAnimation(.easeIn) {
+                                selectedCoin = coin
+                                
+                            }
+                        }
+                        .background(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(selectedCoin?.id == coin.id ?
+                                        Color.theme.darkgreen : Color.clear,
+                                        lineWidth: 1)
+                        )
+                }
+            }
+            .padding(.vertical, 4)
+            .padding(.leading)
+        })
+    }
+    
+    private func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText) {
+            return quantity * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
     }
 }
