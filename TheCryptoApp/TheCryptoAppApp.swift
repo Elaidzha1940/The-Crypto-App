b6a9ddd82e168d3874b545586b6062c5226a3dd8
Elaidzha1940 <eli080293@gmail.com>
2023-11-23 02:39:49 +0300

    Update TheCryptoAppApp

--- TheCryptoApp/TheCryptoAppApp.swift
+++ TheCryptoApp/TheCryptoAppApp.swift
@@ -14,6 +14,11 @@ struct TheCryptoAppApp: App {
     
     @StateObject private var vm = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
