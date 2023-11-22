756f281d4e5bffb041faeb6daf00929e6eedeaba
Elaidzha1940 <eli080293@gmail.com>
2023-11-22 17:24:28 +0300

    Add PortfolioView

---
+++ TheCryptoApp/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,24 @@
+//  /*
+//
+//  Prokect: TheCryptoApp
+//  File: PortfolioView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 22.11.2023
+//
+//  */
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        
+        VStack {
+            
+            Text("Portfolio View")
+        }
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
