f43117503b8743650fb8037da6ef07b964c9afb6
Elaidzha1940 <eli080293@gmail.com>
2023-11-22 19:48:06 +0300

    Add CoinLogoView

---
+++ TheCryptoApp/Core/Components/CoinImage/CoinLogoView.swift
@@ -0,0 +1,42 @@
+//  /*
+//
+//  Project: TheCryptoApp
+//  File: CoinLogoView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 22.11.2023
+//
+//  */
+
+import SwiftUI
+
+struct CoinLogoView: View {
+    let coin: CoinModel
+    
+    var body: some View {
+        
+        VStack {
+            CoinImageView(coin: coin)
+                .frame(width: 50, height: 50)
+            
+            Text(coin.symbol.uppercased())
+                .font(.system(size: 18, weight: .bold, design: .rounded))
+                .foregroundColor(Color.theme.accent)
+                .lineLimit(1)
+                .minimumScaleFactor(0.5)
+            
+            Text(coin.name)
+                .font(.system(size: 14, weight: .medium, design: .rounded))
+                .foregroundColor(Color.theme.secondaryText)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+        }
+    }
+}
+
+struct CoinLogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinLogoView(coin: dev.coin)
+
+    }
+}
--- TheCryptoApp/Core/Home/Views/PortfolioView.swift
+++ TheCryptoApp/Core/Home/Views/PortfolioView.swift
@@ -10,13 +10,22 @@
 import SwiftUI
 
 struct PortfolioView: View {
+    @EnvironmentObject private var vm: HomeViewModel
     
     var body: some View {
         
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
-                    Text("Portfolio View")
+                    SearchBarView(searchText: $vm.searchText)
+                    
+                    ScrollView(.horizontal, showsIndicators: true, content: {
+                        LazyHStack(spacing: 10) {
+                            ForEach(vm.allCoins) { coin in
+                                Text(coin.symbol.uppercased())
+                            }
+                        }
+                    })
                 }
             }
             .navigationTitle("Edit Portfolio")
@@ -29,6 +38,9 @@ struct PortfolioView: View {
     }
 }
 
-#Preview {
-    PortfolioView()
+struct PortfolioView_Previews: PreviewProvider {
+    static var previews: some View {
+        PortfolioView()
+            .environmentObject(dev.HomeVM)
+    }
 }
