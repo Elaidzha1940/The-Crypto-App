7b1af4daf6bf87159b39355200012cb41eef09f1
Elaidzha1940 <eli080293@gmail.com>
2023-11-22 03:30:21 +0300

    Update HomeViewModel ? StatisticModel

--- TheCryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ TheCryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -12,12 +12,7 @@ import Combine
 
 class HomeViewModel: ObservableObject {
     
-    @Published var statistics: [StatisticModel] = [
-        StatisticModel(title: "Title", value: "Value", percentageChange: 10),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value"),
-        StatisticModel(title: "Title", value: "Value", percentageChange: -8),
-    ]
+    @Published var statistics: [StatisticModel] = []
     
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
@@ -47,8 +42,27 @@ class HomeViewModel: ObservableObject {
         // market data service
         marketDataService.$marketData
             .map { (marketDataModel) -> [StatisticModel] in
+                
                 var stats: [StatisticModel] = []
+                
+                guard let data = marketDataModel else {
+                    return stats
+                }
+                
+                let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+                let volume = StatisticModel(title: "24h Volume", value: data.volume)
+                let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+                let portfolio = StatisticModel(title: "Potfolio Value", value: "$0.00", percentageChange: 0)
+                
+                stats.append(contentsOf: [
+                    marketCap, volume, btcDominance, portfolio
+                ])
+                return stats
             }
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
+            }
+            .store(in: &cancellables)
     }
     
     private func filteredCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
