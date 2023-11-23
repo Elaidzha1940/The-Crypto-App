a913346922e14f59ff6e07b8b43c58ec02e562da
Elaidzha1940 <eli080293@gmail.com>
2023-11-23 22:04:33 +0300

    Update PortfolioView

--- TheCryptoApp/Core/Home/Views/PortfolioView.swift
+++ TheCryptoApp/Core/Home/Views/PortfolioView.swift
@@ -38,6 +38,11 @@ struct PortfolioView: View {
                     trailingNavBarButtons
                 }
             })
+            .onChange(of: vm.searchText) { value in
+                if value == "" {
+                    removeSelectedCoin()
+                }
+            }
         }
     }
 }
