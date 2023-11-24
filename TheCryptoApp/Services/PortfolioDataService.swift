cebf68b340811c4905684d44736d937d6afb0df8
Elaidzha1940 <eli080293@gmail.com>
2023-11-24 03:22:23 +0300

    Update PortfolioDataService / getPortfolio()

--- TheCryptoApp/Services/PortfolioDataService.swift
+++ TheCryptoApp/Services/PortfolioDataService.swift
@@ -24,13 +24,33 @@ class PortfolioDataService {
             if let error = error {
                 print("Error loading Core Data. \(error)")
             }
+            self.getPortfolio()
         }
     }
+    
+    // MARK: Public
+    
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+        
+        // check if coin is already in portfolio
+        if let entity = savedEntities.first(where: { $0.coinID == coin.id }) {
+            if amount > 0 {
+                update(entity: entity, amount: amount)
+            } else {
+                delete(entity: entity)
+            }
+        } else {
+            add(coin: coin, amount: amount)
+        }
+    }
+    
+    
+    // MARK: Private
     
     private func getPortfolio() {
         let request = NSFetchRequest<PortfolioEntity>(entityName: containerName)
         do {
-           savedEntities = try container.viewContext.fetch(request)
+            savedEntities = try container.viewContext.fetch(request)
         } catch let error {
             print("Error fetching Portfolio Entities. \(error)")
         }
@@ -40,10 +60,28 @@ class PortfolioDataService {
         let entity = PortfolioEntity(context: container.viewContext)
         entity.coinID = coin.id
         entity.amount = amount
-        
+    }
+    
+    private func update(entity: PortfolioEntity, amount: Double) {
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    private func delete(entity: PortfolioEntity) {
+        container.viewContext.delete(entity)
+        applyChanges()
     }
     
     private func save() {
-        
+        do {
+            try container.viewContext.save()
+        } catch let error {
+            print("Error to Core Data. \(error)")
+        }
+    }
+    
+    private func applyChanges() {
+        save()
+        getPortfolio()
     }
 }
