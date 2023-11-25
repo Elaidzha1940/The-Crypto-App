c4fe6e31cc1bf968b8520345464ebe99e85ca172
Elaidzha1940 <eli080293@gmail.com>
2023-11-25 03:59:38 +0300

    Update DetailViewModel

--- TheCryptoApp/Core/Detail/ViewModels/DetailViewModel.swift
+++ TheCryptoApp/Core/Detail/ViewModels/DetailViewModel.swift
@@ -8,12 +8,25 @@
 //
 
 import Foundation
+import Combine
 
 class DetailViewModel {
     
     private let coinDetailService: CoinDetailDataService
+    private var cancellables = Set<AnyCancellable>()
     
     init(coin: CoinModel) {
         self.coinDetailService = CoinDetailDataService(coin: coin)
+        self.addSubscribers()
+    }
+    
+    private func addSubscribers() {
+        
+        coinDetailService.$coinDetails
+            .sink { (returnedCoinDetails) in
+                print("Recievd coin detail data")
+                print(returnedCoinDetails)
+            }
+            .store(in: &cancellables)
     }
 }
