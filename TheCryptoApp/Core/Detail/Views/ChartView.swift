06c518ee77285fb594467de38406a88164c368a5
Elaidzha1940 <eli080293@gmail.com>
2023-11-26 03:28:37 +0300

    Update ChartView

--- TheCryptoApp/Core/Detail/Views/ChartView.swift
+++ TheCryptoApp/Core/Detail/Views/ChartView.swift
@@ -11,27 +11,23 @@ import SwiftUI
 
 struct ChartView: View {
     let data: [Double]
+    let maxY: Double
+    let minY: Double
+    let lineColor: Color
     
     init(coin: CoinModel) {
         data = coin.sparklineIn7D?.price ?? []
+        maxY = data.max() ?? 0
+        minY = data.min() ?? 0
+        
+        let priceChange = (data.last ?? 0) - (data.first ?? 0)
+        lineColor = priceChange > 0 ? Color.theme.darkgreen : Color.theme.darkred
     }
     
     var body: some View {
         
-        GeometryReader { geometry in
-            Path { path in
-                for index in data.indices {
-                    
-                    let xPosition = geometry.size.width / CGFloat(data.count) *
-                    CGFloat(index + 1)
-                    
-                    if index == 0 {
-                        path.move(to: CGPoint(x: 0, y: 0))
-                    }
-                    path.addLine(to: CGPoint(x: xPosition, y: 0))
-                }
-            }
-            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        VStack {
+            chartView
         }
     }
 }
@@ -39,5 +35,30 @@ struct ChartView: View {
 struct ChartView_Previews: PreviewProvider {
     static var previews: some View {
         ChartView(coin: dev.coin)
+    }
+}
+
+extension ChartView {
+    
+    private var chartView: some View {
+        GeometryReader { geometry in
+            Path { path in
+                for index in data.indices {
+                    
+                    let xPosition = geometry.size.width / CGFloat(data.count) *
+                    CGFloat(index + 1)
+                    
+                    let yAxis = maxY - minY
+                    
+                    let yPosition = (1 - CGFloat((data[index] - minY) / yAxis)) * geometry.size.height
+                    
+                    if index == 0 {
+                        path.move(to: CGPoint(x: xPosition, y: yPosition))
+                    }
+                    path.addLine(to: CGPoint(x: xPosition, y: yPosition))
+                }
+            }
+            .stroke(lineColor, style: StrokeStyle(lineWidth: 3 , lineCap: .round, lineJoin: .round))
+        }
     }
 }
