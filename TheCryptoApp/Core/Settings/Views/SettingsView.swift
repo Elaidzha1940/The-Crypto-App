ecbc5d60b4b407369f744a16c9484070305fbe0e
Elaidzha1940 <eli080293@gmail.com>
2023-11-26 19:39:36 +0300

    Add SettingsView

---
+++ TheCryptoApp/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: TheCryptoApp
+//  File: SettingsView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 26.11.2023
+//
+//  */
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    SettingsView()
+}
