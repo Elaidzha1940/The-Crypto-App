cd94b5ae514242ececfa468e106ec795d15ca4ed
Elaidzha1940 <eli080293@gmail.com>
2023-11-27 08:07:58 +0300

    Add LaunchView and still fixing a LaunchScreen

---
+++ TheCryptoApp/Core/Launch/Views/LaunchView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: TheCryptoApp
+//  File: LaunchView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 27.11.2023
+//
+//  */
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    LaunchView()
+}
